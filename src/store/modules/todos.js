4be5fe2d31284bee863bba50e94c0da48313c03a
Eddie <coloed3@gmail.com>
2019-11-08 11:23:27 -0700

    first commit

---
+++ src/store/modules/todos.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+import Axios from 'axios';
+
+const state = {
+  todos: []
+};
+
+const getters = {
+  allTodos: state => state.todos
+};
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await Axios.get(
+      'https://jsonplaceholder.typicode.com/todos'
+    );
+    console.log(response.data);
+    commit('setTodos', response.data);
+  },
+  async addTodo({ commit }, title) {
+    const response = await Axios.post(
+      'https://jsonplaceholder.typicode.com/todos',
+      { title, completed: false }
+    );
+
+    commit('newTodo', response.data);
+
+    console.log(response);
+  }
+};
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo)
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
